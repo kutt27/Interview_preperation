4431c1c8f1e411f0d49d74d598a3b9d584f793ef
kutt27 <satheesanamal6@gmail.com>
2023-06-13 09:39:35 +0530

    Created a new folder called Variables and added the boiler plate code

---
+++ Variables/BoilerPlate.java
@@ -0,0 +1,8 @@
+// class name would be same as file name
+public class BoilerPlate {
+    public static void main(String[] args) {
+        System.out.println("Nothing");
+    }
+}
+
+// This code should be written for any java file. This code is known as 'Boilder Plate Code'
