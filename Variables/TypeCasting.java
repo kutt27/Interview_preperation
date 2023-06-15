2bc92d38f0756563f313cf3f734ac5c9930e6c9f
kutt27 <satheesanamal6@gmail.com>
2023-06-15 22:35:37 +0530

    Added narrow casting

---
+++ Variables/TypeCasting.java
@@ -0,0 +1,13 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Casting higher data to lower data
+        float a = 25.0000F;
+        int b = (int) a;
+        System.out.println(b);
+
+        // ascii value to integer
+        char ch = 'b';
+        int x = ch;
+        System.out.println(x);
+    }
+}
